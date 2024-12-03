6cbf0d40fdb5f00a64a05d28b361b5ecec23a257
klaudiamr97 <klaudia.mr97@gmail.com>
2024-12-03 15:58:39 +0000

    Updated next.config.mjs

--- src/components/BrandShowcase.jsx
+++ src/components/BrandShowcase.jsx
@@ -1,6 +1,23 @@
 
+const achievements = [
+  {
+    label: "Commitment to Sustainability:",
+    description:
+      "Your focus on organic, locally sourced ingredients is something we deeply admire, setting a new standard in the plant-based dining industry.",
+  },
+  {
+    label: "Innovative Menu:",
+    description:
+      "From your mouthwatering vegan tacos to decadent desserts, every dish is a celebration of flavor and ethical dining.",
+  },
+  {
+    label: "Connection to Community:",
+    description:
+      "Your dedication to supporting local farmers and reducing your environmental footprint shows how much you care about both people and the planet.",
+  },
+];
 
-function BrandShowcase() {
+export default function BrandShowcase() {
     return (
       <div className="flex flex-col md:flex-row md:space-x-12 mx-8 mt-10 md:mx-10 md:mt-16 lg:mx-20 lg:mt-24">
         <h2 className="text-h2 mb-4 md:mb-0 md:basis-1/2">What We Love About Veganic</h2>
@@ -16,23 +33,5 @@ function BrandShowcase() {
     );
   }
   
-  const achievements = [
-    {
-      label: "Commitment to Sustainability:",
-      description:
-        "Your focus on organic, locally sourced ingredients is something we deeply admire, setting a new standard in the plant-based dining industry.",
-    },
-    {
-      label: "Innovative Menu:",
-      description:
-        "From your mouthwatering vegan tacos to decadent desserts, every dish is a celebration of flavor and ethical dining.",
-    },
-    {
-      label: "Connection to Community:",
-      description:
-        "Your dedication to supporting local farmers and reducing your environmental footprint shows how much you care about both people and the planet.",
-    },
-  ];
-  
-  export default BrandShowcase;
+
   
