e7eeb5d404bbad8b8674ead93ff9039b9e233236
klaudiamr97 <klaudia.mr97@gmail.com>
2024-12-30 13:33:21 +0000

    Improved error handling on api call

--- src/components/CaseStudies.jsx
+++ src/components/CaseStudies.jsx
@@ -8,17 +8,7 @@ export default function CaseStudies(props) {
   let { data, loading, error } = useFetchData('https://67560b6111ce847c992bd68c.mockapi.io/case-studies/Casestudies');
 
   return error ? (
-    <div>
-      <ErrorMessage>
-        Oops! It looks like the case studies are missing.
-        <button
-          className="bg-button-bg text-button hover:bg-white border-2 hover:text-button-hover border-button-bg px-8 md:px-16 py-2 rounded-md my-4 transition ease-in-out"
-          onClick={handleRetry}
-        >
-          Try again
-        </button>
-      </ErrorMessage>
-    </div>
+    <ErrorMessage>Oops! It looks like the case studies are missing.</ErrorMessage>
   ) : (
     <div className="flex flex-col mx-8 mt-10 md:mx-10 md:mt-16 lg:mx-20 lg:mt-24">
       <div>
--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export function ErrorMessage({ children }) {
-  return <p className="flex flex-col text-h2 mx-4 mt-10 md:mt-16 items-center text-center">{children}</p>;
+  return (
+    <div className="flex flex-col text-h2 mx-4 mt-10 md:mt-16 items-center text-center">
+      <p>{children}</p>
+      <button
+        className="bg-button-bg text-button hover:bg-white border-2 hover:text-button-hover border-button-bg px-8 md:px-16 py-2 rounded-md my-4 transition ease-in-out"
+        onClick={() => window.location.reload()}
+      >
+        Try again
+      </button>
+    </div>
+  );
 }
