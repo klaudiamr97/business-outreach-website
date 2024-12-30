f6b19aabf1eb96229a19db1445afa2eaeec867c4
klaudiamr97 <klaudia.mr97@gmail.com>
2024-12-30 16:33:12 +0000

    Adjusted margin and padding on ImageText component

--- src/components/ImageText.jsx
+++ src/components/ImageText.jsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function ImageText(props) {
   const { imgSrc, children } = props;
   return (
-    <div className="flex flex-col md:flex-row mx-8 md:mx-10 lg:mx-20 mt-10 md:mt-16 lg:mt-24 space-y-8 md:space-y-0 md:space-x-12">
-      <div className="md:basis-1/2 flex md:items-center">
+    <div className="flex flex-col md:flex-row mx-8 md:mx-10 lg:mx-20 mt-10 md:mt-16 lg:mt-20 space-y-8 md:space-y-0 md:space-x-12">
+      <div className="md:basis-1/2 flex md:items-center lg:pr-16">
         <div>{children}</div>
       </div>
       <div className="relative w-full md:basis-1/2 flex-grow">
