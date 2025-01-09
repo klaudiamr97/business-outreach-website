3f1fd044bc97cd5cc8bd22008025e2b0b903d0e6
klaudiamr97 <klaudia.mr97@gmail.com>
2025-01-09 16:11:43 +0000

    Updated max width

--- src/components/ImageText.jsx
+++ src/components/ImageText.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function ImageText(props) {
   const { imgSrc, children } = props;
   return (
-    <div className=" flex flex-col  md:flex-row mx-8 md:mx-10 lg:mx-20 mt-10 md:mt-16 lg:mt-10 space-y-8 md:space-y-0 md:space-x-12">
+    <div className=" flex flex-col md:flex-row mx-8 md:mx-10 lg:mx-20 mt-10 md:mt-16 lg:mt-10 space-y-8 md:space-y-0 md:space-x-12">
       <div className="md:basis-1/2 flex md:items-center lg:pr-16">
         <div>{children}</div>
       </div>
